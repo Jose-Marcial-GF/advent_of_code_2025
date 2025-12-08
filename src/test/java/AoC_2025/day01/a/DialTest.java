99c427f1e71b108418be4dcbd4bcafdef3ff4123
4w4kt <carrona43@gmail.com>
2025-12-08 20:35:06 +0000

    first green

--- src/test/java/AoC_2025/day01/a/DialTest.java
+++ src/test/java/AoC_2025/day01/a/DialTest.java
@@ -7,10 +7,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DialTest {
     @Test
     public void should_move_given_a_direction() {
-        assertThat(snake.move("L1").position()).isEqualto(49);
+        assertThat(Snake.create().move("L1")).isEqualTo(49);
     }
 
-    public static class snake{
+    public static class Snake {
+        private final int position;
 
+        private Snake(int position) {
+            this.position = position;
+        }
+
+        private static Snake create(){
+            return new Snake(50);
+        }
+
+        public int move(String direction) {
+            return position + toInt(direction);
+        }
+
+        private int toInt(String direction) {
+            return Integer.parseInt(direction.replaceAll("L", "-"));
+        }
     }
 }
