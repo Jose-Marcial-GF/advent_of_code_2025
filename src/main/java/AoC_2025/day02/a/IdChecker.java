0e99299c75988c0a0ce7e2a550250883f91e8eb8
4w4kt <carrona43@gmail.com>
2025-12-31 14:34:30 +0000

    day 2.a implemented

--- src/main/java/AoC_2025/day02/a/IdChecker.java
+++ src/main/java/AoC_2025/day02/a/IdChecker.java
@@ -1,18 +1,19 @@
 package AoC_2025.day02.a;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.stream.LongStream;
 
 class IdChecker {
-    public static int getInvalidCodesSum(String s) {
-        return getInvalidCodes(s.split("-")).sum();
+    public static Long getInvalidCodesSum(String s) {
+        return Arrays.stream(s.split(",")).mapToLong(s1-> getInvalidCodes(s1.split("-")).sum()).sum();
     }
 
-    private static IntStream getInvalidCodes(String[] split) {
-        return InvalidIdProvider.getInvalidCodes(toInt(split[0]), toInt(split[1]));
+    private static LongStream getInvalidCodes(String[] split) {
+        return InvalidIdProvider.getInvalidCodes(toLong(split[0]), toLong(split[1]));
     }
 
-    private static Integer toInt(String split) {
-        return Integer.parseInt(split);
+    private static Long toLong(String split) {
+        return Long.parseLong(split);
     }
 }
 
--- src/main/java/AoC_2025/day02/a/InvalidIdProvider.java
+++ src/main/java/AoC_2025/day02/a/InvalidIdProvider.java
@@ -1,24 +1,24 @@
 package AoC_2025.day02.a;
 
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 class InvalidIdProvider {
 
-    public static IntStream getInvalidCodes(int min, int max) {
-        return IntStream.range(min, max).mapToObj(String::valueOf).mapToInt(InvalidIdProvider::check).filter(i -> i >= 0);
+    public static LongStream getInvalidCodes(Long min, Long max) {
+        return LongStream.range(min, max + 1).mapToObj(String::valueOf).mapToLong(InvalidIdProvider::check).filter(i -> i > 0);
     }
 
-    private static int check(String s) {
+    private static Long check(String s) {
         return check(s.substring(s.length() / 2), s.substring(0, s.length() / 2));
     }
 
-    private static int check(String substring, String substring1) {
-        if (substring.equals(substring1)) return toInt(substring1);
-        return 0;
+    private static Long check(String substring, String substring1) {
+        if (substring.equals(substring1)) return toInt(substring + substring1);
+        return 0L;
     }
 
-    private static Integer toInt(String split) {
-        return Integer.parseInt(split);
+    private static Long toInt(String split) {
+        return Long.parseLong(split);
     }
 
 }
