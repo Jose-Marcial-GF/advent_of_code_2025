7878488b2d00096f2f053af8fbbee91fae8460b5
4w4kt <carrona43@gmail.com>
2025-12-31 15:02:12 +0000

    day 2.a and 2.b refactorized

--- src/main/java/AoC_2025/day02/a/InvalidIdProvider.java
+++ src/main/java/AoC_2025/day02/a/InvalidIdProvider.java
@@ -5,19 +5,18 @@ import java.util.stream.LongStream;
 class InvalidIdProvider {
 
     public static LongStream getInvalidCodes(Long min, Long max) {
-        return LongStream.range(min, max + 1).mapToObj(String::valueOf).mapToLong(InvalidIdProvider::check).filter(i -> i > 0);
+        return LongStream.range(min, max + 1).mapToObj(String::valueOf).filter(InvalidIdProvider::isValid).mapToLong(InvalidIdProvider::toLong).filter(i -> i > 0);
     }
 
-    private static Long check(String s) {
-        return check(s.substring(s.length() / 2), s.substring(0, s.length() / 2));
+    private static boolean isValid(String s) {
+        return isValid(s.substring(s.length() / 2), s.substring(0, s.length() / 2));
     }
 
-    private static Long check(String substring, String substring1) {
-        if (substring.equals(substring1)) return toInt(substring + substring1);
-        return 0L;
+    private static boolean isValid(String substring, String substring1) {
+        return substring.equals(substring1);
     }
 
-    private static Long toInt(String split) {
+    private static Long toLong(String split) {
         return Long.parseLong(split);
     }
 
