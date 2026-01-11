c0b209a125c7b65a8b264b8e20023d6962996eb9
4w4kt <carrona43@gmail.com>
2026-01-11 03:48:04 +0000

    10a implemented

--- src/main/java/AoC_2025/day10/a/State.java
+++ src/main/java/AoC_2025/day10/a/State.java
@@ -3,28 +3,25 @@ package AoC_2025.day10.a;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
-public record State(List<Integer> Lights) {
+public record State(List<Integer> lights) {
 
     public static State initial(int size) {
         return new State(Collections.nCopies(size, 0));
     }
 
     public int size() {
-        return Lights.size();
+        return lights.size();
     }
 
     public State apply(Operator op) {
-        List<Integer> newLights = new ArrayList<>(this.Lights);
+        return new State(IntStream.range(0, lights.size())
+                .mapToObj(i -> op.lights().contains(i)
+                        ? lights.get(i) ^ 1
+                        : lights.get(i))
+                .toList());
 
-        // Iteramos sobre los índices que toca el operador
-        for (Integer index : op.lights()) {
-            if (index >= 0 && index < newLights.size()) {
-                // Operación XOR: 1^1=0, 0^1=1 (Toggle)
-                int currentVal = newLights.get(index);
-                newLights.set(index, currentVal ^ 1);
-            }
-        }
-        return new State(newLights);
     }
+
 }
